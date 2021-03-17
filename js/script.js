269e4e1d157878edbf717d54de9cfda02b678ed7
Herik Martins <herikcosmo.m@gmail.com>
2021-03-17 12:55:54 -0300

    Prototipo do site

--- js/script.js
+++ js/script.js
@@ -1,16 +1,39 @@
-let key = 'item 1';
+document.getElementById('button').addEventListener("click", function () {
+    let key = document.getElementById('key').value
+    let value = document.getElementById('value').value
+    localStorage.setItem(key, value)
+    atualiza()
+})
 
-let nota1 = {title: 'Nota1', description: 'blablbal'}
-let nota2 = {title: 'Nota2', description: 'dsmkds'}
+window.addEventListener('storage', function(event){
+    let key = event.key
+    let newValue = event.newValue
+    let oldValue = event.oldValue
+    let storageArea = event.storageArea
+    key.innerHTML(key+"\n"+newValue+"\n"+oldValue+"\n"+storageArea)
+    atualiza()
+})
 
+let colors = ['#aed143', '#fbd249', '#fbd249', '#d35595', '#d35595']
 
-localStorage.setItem(1, JSON.stringify(nota1));
-localStorage.setItem(2, JSON.stringify(nota2));
+function atualiza(){
 
-
-for (let i = 0; i < localStorage.length; i++){
-    let key = localStorage.key(i);
-    let value = JSON.parse(localStorage.getItem(key));
-    console.log(value['title'], value['description'])
+    document.getElementById('content').innerHTML = ''
+    for (let i = 0; i < localStorage.length; i++){
+    let key = localStorage.key(i)
+    let value = localStorage.getItem(key)
+    conteudo = `<div class='post-it'><h1>${key}</h1><p>${value}</p></div>`
+    document.getElementById('content').innerHTML += conteudo
+    let num = Math.ceil(Math.random()* colors.length - 1)
+    document.getElementsByClassName('post-it')[i].style.backgroundColor = colors[num]
+    }
+    
 }
 
+
+document.getElementById('delete').addEventListener('click', function (){
+    localStorage.clear()  
+    atualiza()
+})
+
+atualiza()
