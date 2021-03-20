8899110ad590cf906f85aa594798f711815d6d7b
Herik Martins <herikcosmo.m@gmail.com>
2021-03-20 19:06:53 -0300

    Funcao delete

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@ window.addEventListener('load', function(){
     let openModal = document.getElementById("open-modal")
     let closeModal = document.getElementById("close-modal")
     let modal = document.getElementsByClassName('modal')[0]
+    let keyNote = document.getElementsByClassName('idNote')
     list()
 
     openModal.addEventListener('click', function(){
@@ -21,6 +22,7 @@ window.addEventListener('load', function(){
         let key = "notes"
         let newNote = note.value.replaceAll('\n','<br>')
         notes.push(newNote)
+        console.log(notes)
         let value = JSON.stringify(notes)
         localStorage.setItem(key, value)
         note.value = ''
@@ -34,9 +36,12 @@ window.addEventListener('load', function(){
             for(i = 0; i < total.length; i++){
                 content.innerHTML += `
                 <div class="post-it">
-                    <h3>${total[i].replace('↵', '<br>')}</h3>
+                    <h3>${total[i]}</h3>
+                    <input type='hidden' class='idNote' value='${i}'>
+                    <button onclick='deleteNote(${i})'>delete</button>
                 </div>`
             }
+            
         } 
     }
 
@@ -45,6 +50,14 @@ window.addEventListener('load', function(){
         notes = []
         list()
     })
+
+    deleteNote = function(value){
+        notes.splice(value, 1)
+        localStorage.setItem("notes", JSON.stringify(notes))
+        list()
+    }
+
+    
 
     // document.addEventListener('storage')
     
